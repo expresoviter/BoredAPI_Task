e4c91f3148b70361b3a3fd0f7075c41936b9c747
expresoviter <lesivvladyslav@gmail.com>
2023-09-05 13:32:37 +0300

    Completed a command line program, some local improvements.

---
+++ my_program.py
@@ -0,0 +1,27 @@
+import argparse
+import wrapper
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    subparsers = ap.add_subparsers(help="Commands `new` and `list`", dest="command")
+
+    newParser = subparsers.add_parser("new")
+    listParser = subparsers.add_parser("list")
+
+    newParser.add_argument("-t", "--type", help="Type of an activity")
+    newParser.add_argument("-p", "--participants", help="Number of participants")
+    newParser.add_argument("-pm", "--minprice", help="Minimal price")
+    newParser.add_argument("-px", "--maxprice", help="Maximal price")
+    newParser.add_argument("-am", "--minaccessibility", help="Minimal accessibility")
+    newParser.add_argument("-ax", "--maxaccessibility", help="Maximal accessibility")
+
+    args = ap.parse_args().__dict__
+    command = args['command']
+    del args['command']
+    saver = wrapper.ActivitySaver()
+    if command == "new":
+        api = wrapper.APIWrapper()
+        result = api.request(args)
+        saver.saveActivity(result)
+    elif command == "list":
+        saver.getLatestActivities()
--- wrapper.py
+++ wrapper.py
@@ -1,14 +1,15 @@
 import requests
-import psycopg2
-from database import *
 
 
 class APIWrapper:
     def __init__(self):
         self.url = "http://www.boredapi.com/api/activity/"
 
-    def request(self, **kwargs):
-        PARAMS = kwargs
+    def request(self, kwargs):
+        PARAMS = dict()
+        for i in kwargs:
+            if not kwargs[i] is None:
+                PARAMS[i]=kwargs[i]
         possibleKeys = {'key', 'type', 'participants', 'price', 'minprice', 'maxprice', 'accessibility',
                          'minaccessibility', 'maxaccessibility'}
         incorrectKeys = kwargs.keys() - possibleKeys
@@ -17,13 +18,4 @@ class APIWrapper:
         R = requests.get(url=self.url, params=PARAMS)
         DATA = R.json()
         return DATA
-
-
-if __name__ == "__main__":
-    api = APIWrapper()
-    result = api.request(type="social", participants=1)
-    print(result)
-    saver = ActivitySaver()
-    #saver.saveActivity(result)
-    saver.getLatestActivities()
 
